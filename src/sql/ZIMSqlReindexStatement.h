c711c537e84b368183ca239c859353fe2c1483ea
Ziminji <ziminji@gmail.com>
2011-06-26 02:38:04 -0600

    Adding more builder classes for the explain and reindex statements.

---
+++ src/sql/ZIMSqlReindexStatement.h
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2011 Ziminji
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ * 
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#import "ZIMSqlStatement.h"
+
+/*!
+ @class					ZIMSqlReindexStatement
+ @discussion			This class represents an SQL reindex statement.
+ @version				2011-06-26
+ @see					http://www.sqlite.org/lang_reindex.html
+ */
+@interface ZIMSqlReindexStatement : NSObject <ZIMSqlStatement> {
+
+	@protected
+		NSString *_table;
+
+}
+/*!
+ @method				table:
+ @discussion			This method will set the table used in the SQL statement.
+ @param table			The table that will be used in the SQL statement.
+ @version				2011-06-26
+ */
+- (void) table: (NSString *)table;
+/*!
+ @method				statement
+ @discussion			This method will return the SQL statement.
+ @return				The SQL statement that was constructed.
+ @version				2011-06-26
+ */
+- (NSString *) statement;
+
+@end
