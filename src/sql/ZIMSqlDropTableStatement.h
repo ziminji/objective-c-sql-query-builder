3474f6ed40ffdb29a20098352d646865b2e6f135
Ziminji <ziminji@gmail.com>
2011-07-23 05:40:41 -0600

    Added more XML to DDL handling and improved some of the XML parsing.

--- src/sql/ZIMSqlDropTableStatement.h
+++ src/sql/ZIMSqlDropTableStatement.h
@@ -20,16 +20,37 @@
 /*!
  @class					ZIMSqlDropTableStatement
  @discussion			This class represents an SQL drop table statement.
- @updated				2011-07-15
+ @updated				2011-07-23
  @see					http://www.sqlite.org/lang_droptable.html
  */
-@interface ZIMSqlDropTableStatement : NSObject <ZIMSqlStatement, ZIMSqlDataDefinitionCommand> {
+@interface ZIMSqlDropTableStatement : NSObject <ZIMSqlStatement, ZIMSqlDataDefinitionCommand, NSXMLParserDelegate> {
 
 	@protected
 		NSString *_table;
 		BOOL _exists;
+        //NSInteger _depth;
+		NSInteger _counter;
+		NSError **_error;
 
 }
+/*!
+ @method				initWithXML:error:
+ @discussion			This method initializes the class via an XML file following Torque's "XML to DDL" schema.
+ @param xml	            The UTF-8 encoded string of XML.
+ @param error           Used when an error occurs while processing the XML data. May be NULL.
+ @return                An instance of this class.
+ @updated				2011-07-23
+ @see					http://db.apache.org/ddlutils/
+ @see					http://db.apache.org/ddlutils/schema/
+ */
+- (id) initWithXML: (NSData *)xml error: (NSError **)error;
+/*!
+ @method				init
+ @discussion			This method initializes the class.
+ @return                An instance of this class.
+ @updated				2011-07-23
+ */
+- (id) init;
 /*!
  @method				table:
  @discussion			This method will set the table used in the SQL statement.
